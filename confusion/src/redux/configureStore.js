afe75370438ca51a2659270e717dc02809a01e6e
Robin_Chen <sudacgb@gmail.com>
2022-04-19 07:55:49 -0400

    combine reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState} from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from '../redux/dishes';
+import { Comments } from '../redux/comments';
+import { Promotions } from '../redux/promotions';
+import { Leaders } from '../redux/leaders';
+
 
 export const ConfigureStore = () => {
 	const store = createStore(
-		Reducer,
-		initialState
+
+		combineReducers({
+			dishes: Dishes,
+			comments: Comments,
+			promotions: Promotions,
+			leaders: Leaders
+		})
 	);
 
 	return store;
